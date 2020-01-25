d6b91d4ee125349a0a2981bfeec7ef82eb5a7a15
Sly Gryphon <sly.gryphon@readify.net>
2020-01-25 21:20:41 +1000

    Tweak initial build file

--- bindings/c/Make.cmd
+++ bindings/c/Make.cmd
@@ -7,13 +7,12 @@ set CORE_DIR=%ROOT_DIR%/core
 set BIND_DIR=%ROOT_DIR%/bindings
 set CBIND_DIR=%BIND_DIR%/c
 
-set CFLAGS=
+set CFLAGS=/W4 /O2 /EHsc
 set LFLAGS=
 set INCLUDES=
 set OBJ=%OUT_DIR%/mothra-ingress.obj
 set TARGET=%OUT_DIR%/mothra-ingress.%EXT%
 
-if not exist %OUT_DIR% ( mkdir %OUT_DIR% )
-if exist %TARGET% ( del %TARGET% )
-cl /c %CFLAGS% %CBIND_DIR%/mothra.c /Fo%OBJ%
-link /DLL /OUT:%target% %OBJ%
+if not exist "%OUT_DIR%" ( mkdir "%OUT_DIR%" )
+if exist "%TARGET%" ( del "%TARGET%" )
+cl /LD %CFLAGS% "%CBIND_DIR%/mothra.c" /Fe"%TARGET%" /Fo"%OBJ%"
